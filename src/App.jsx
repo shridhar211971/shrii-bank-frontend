6eff1cf9c7b702047d75d885e2ec20899c320713
shridhar-indi4 <shridhar.bhoslae@indi4.io>
2026-05-18 14:55:55 +0530

    add all project and folder stucture

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-[#050816] flex items-center justify-center">
+      <h1 className="text-6xl font-bold text-cyan-400">
+        SHRII BANK
+      </h1>
+    </div>
+  );
+}
+
+export default App;
